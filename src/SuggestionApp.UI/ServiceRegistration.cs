d6fafb41d106f4f058f1ba56bfceebd00aea6b7a
James Alanis <fjam07@gmail.com>
2023-09-27 05:43:06 -0500

    Updated DI and added proj ref

--- src/SuggestionApp.UI/ServiceRegistration.cs
+++ src/SuggestionApp.UI/ServiceRegistration.cs
@@ -1,3 +1,7 @@
+using SuggestionApp.Core.Entities;
+using SuggestionApp.Core.Interfaces;
+using SuggestionApp.Repository;
+
 namespace SuggestionApp.UI;
 
 public static class ServiceRegistration 
@@ -8,5 +12,12 @@ public static class ServiceRegistration
         builder.Services.AddRazorPages();
         builder.Services.AddServerSideBlazor();
         builder.Services.AddMemoryCache();
+
+        builder.Services.AddSingleton<IDbConnection, MongoDbConnection>();
+        builder.Services.AddSingleton<IBaseRepository<Category>, CategoryRepository>();
+        builder.Services.AddSingleton<IBaseRepository<Status>, StatusRepository>();
+        builder.Services.AddSingleton<IBaseRepository<Suggestion>, SuggestionRepository>();
+        builder.Services.AddSingleton<ISuggestionRepository, SuggestionRepository>();
+        builder.Services.AddSingleton<IBaseRepository<User>, UserRepository>();
     }
 }
