e3f2dc8662c7cc8b1e2ed0b6e233bc5baa80ecbc
James Alanis <fjam07@gmail.com>
2023-10-02 02:41:00 -0500

    Added get

--- src/SuggestionApp.Core/Interfaces/ISuggestionService.cs
+++ src/SuggestionApp.Core/Interfaces/ISuggestionService.cs
@@ -3,6 +3,7 @@
 public interface ISuggestionService
 {
     void AuthorSuggestion(Suggestion suggestion, User user);
+    Task<Suggestion> Get(string id);
     Task<IEnumerable<Suggestion>> GetApprovedForRelease();
     Task<IEnumerable<Suggestion>> GetWaitingForApproval();
     bool Vote(Suggestion suggestion, User user, string votingUserId);
