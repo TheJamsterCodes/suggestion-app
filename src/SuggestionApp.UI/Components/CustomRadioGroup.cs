9ab54284482d7422539d0bd9d065b1d9a36ab15d
James Alanis <fjam07@gmail.com>
2023-10-02 13:32:56 -0500

    Created create suggestion view and custom comp

---
+++ src/SuggestionApp.UI/Components/CustomRadioGroup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace SuggestionApp.UI.Components;
+
+public class CustomRadioGroup<TValue> : InputRadioGroup<TValue>
+{
+    private string _fieldclass;
+    private string _name;
+
+    protected override void OnParametersSet()
+    {
+        string fieldClass = EditContext?.FieldCssClass(FieldIdentifier) ?? string.Empty;
+
+        if (fieldClass != _fieldclass || Name != _name)
+        {
+            _fieldclass = fieldClass;
+            _name = Name;
+            base.OnParametersSet();
+        }
+    }
+}
